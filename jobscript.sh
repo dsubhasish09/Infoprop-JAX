723416817bc32b94b80bafa316fa2358002a5d61
Devdutt Subhasish <devdutt.subhasish@rwth-aachen.de>
2026-05-19 17:06:01 +0200

    wheelbot_single_env_refactor_complete

--- jobscript.sh
+++ jobscript.sh
@@ -7,8 +7,8 @@
 #SBATCH --output=output_.out
 #SBATCH --account=rwth2087
 #SBATCH --gres=gpu:1
-#SBATCH --time=01:30:00
-#SBATCH --array=1-4
+#SBATCH --time=02:00:00
+#SBATCH --array=1-5
 
 export CUDA_VISIBLE_DEVICES=0
 
@@ -18,17 +18,14 @@ module load FFmpeg
 export MUJOCO_GL=egl
 
 python -m wheelbot_sim_python.main \
-    experiment=testing_ignore \
+    algorithm=infoprop \
+    experiment=default_run_refactored \
     seed=$RANDOM \
-    algorithm.num_model_envs=10000 \
+    algorithm.num_model_envs=1000 \
     algorithm.grad_updates_per_model_step=10 \
     algorithm.reset_agent_per_trial=False \
-    algorithm.reset_model_replay_buffer=False \
+    algorithm.reset_model_replay_buffer=True \
     algorithm.reset_model_per_trial=False \
     env.rew_scale=1.0 \
     algorithm.tau=0.005 \
-    algorithm.alpha=0.1 \
-    # algorithm.agent_learning_rate=3e-5 \
-    # algorithm.target_entropy=-3 \
-    # algorithm.tune_entropy=True
-
+    algorithm.alpha=0.1
--- wheelbot_sim_python/main.py
+++ wheelbot_sim_python/main.py
@@ -9,23 +9,17 @@ Video evaluation:
     python -m wheelbot_sim_python.main video_eval=true eval.log_dir=... eval.iteration=3 eval.track_seed=42
 """
 
-import jax
 import omegaconf
 import hydra
 from wheelbot_sim_python.training_scripts.brax_infoprop_train import main as infoprop_main
-from wheelbot_sim_python.training_scripts.brax_infoprop_nopmap_train import main as infoprop_nopmap_main
 
 @hydra.main(config_path="config", config_name="main", version_base=None)
 def main(cfg: omegaconf.DictConfig):
     if cfg.video_eval:
         from wheelbot_sim_python.eval_scripts.video_eval import run as video_eval_run
         video_eval_run(cfg.eval)
-    elif cfg.algorithm.name == 'infoprop':
-        infoprop_main(cfg)
-    elif cfg.algorithm.name == 'infoprop_nopmap':
-        infoprop_nopmap_main(cfg)
     else:
-        raise ValueError(f"Unsupported algorithm: {cfg.algorithm.name}")
+        infoprop_main(cfg)
 
 
 if __name__ == "__main__":
